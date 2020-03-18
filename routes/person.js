519a8e94b4a533581ffd3997c0c351ab31c34154
Wessel Smit <wrsmit00@gmail.com>
2020-03-18 23:48:01 +0100

    new page styled + responsive + tabbable

--- routes/person.js
+++ routes/person.js
@@ -1,5 +1,7 @@
 module.exports = (req, res) => {
 	const key = req.body.key
-	console.log('start questionnaire', key)
-
+	console.log('start questionnaire, with key:', key)
+	res.render('person', {
+		key
+	})
 }
