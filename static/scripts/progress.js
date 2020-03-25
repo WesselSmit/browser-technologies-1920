6a351b64a5b3ef2b99e22f7e5f148b0f33ecdd29
Wessel Smit <wrsmit00@gmail.com>
2020-03-25 21:11:53 +0100

    added localStorage feature detection

--- static/scripts/progress.js
+++ static/scripts/progress.js
@@ -1,4 +1,11 @@
-(function () {
+if (localStorageAvailable()) {
+	console.log('localStorage is supported and available')
+	update()
+} else {
+	console.log('localStorage is not supported or not available')
+}
+
+function update() {
 	const form = document.querySelector('form')
 	const allInputs = form.querySelectorAll('input:not([type=hidden]):not([type=submit]), textarea, select')
 	const key = form.querySelector('input[type=hidden').value
@@ -18,7 +25,7 @@
 	}, 250)
 
 	allInputs.forEach(input => input.addEventListener('input', onInput))
-}())
+}
 
 
 
@@ -51,10 +58,38 @@ function debounce(func, wait, immediate) {
 		var later = function () {
 			timeout = null
 			if (!immediate) func.apply(context, args)
-		};
+		}
 		var callNow = immediate && !timeout
 		clearTimeout(timeout)
 		timeout = setTimeout(later, wait)
 		if (callNow) func.apply(context, args)
-	};
-};
+	}
+}
+
+
+
+/* Best way to detect if localStorage is supported & available (taken from MDN: 
+	https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Testing_for_availability) */
+function localStorageAvailable() {
+	let storage
+	try {
+		storage = window['localStorage']
+		const x = '__storage_test__'
+		storage.setItem(x, x)
+		storage.removeItem(x)
+		return true;
+	} catch (err) {
+		return err instanceof DOMException && (
+				// everything except Firefox
+				err.code === 22 ||
+				// Firefox
+				err.code === 1014 ||
+				// test name field too, because code might not be present
+				// everything except Firefox
+				err.name === 'QuotaExceededError' ||
+				// Firefox
+				err.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+			// acknowledge QuotaExceededError only if there's something already stored
+			(storage && storage.length !== 0)
+	}
+}
