778b74752684ce27ac4fba7e10737457a87a47a9
Wessel Smit <wrsmit00@gmail.com>
2020-03-25 21:01:09 +0100

    added debounce to updateSession function

--- static/scripts/progress.js
+++ static/scripts/progress.js
@@ -1,20 +1,31 @@
-const form = document.querySelector('form')
-const allInputs = form.querySelectorAll('input:not([type=hidden]):not([type=submit]), textarea, select')
-const key = form.querySelector('input[type=hidden').value
+(function () {
+	const form = document.querySelector('form')
+	const allInputs = form.querySelectorAll('input:not([type=hidden]):not([type=submit]), textarea, select')
+	const key = form.querySelector('input[type=hidden').value
 
-allInputs.forEach(input => input.addEventListener('input', e => {
-	const page = form.id
-	const name = e.target.name
-	const value = e.target.value
-	const data = {
-		key,
-		page,
-		name,
-		value
-	}
+	const onInput = debounce((e) => {
+		const page = form.id
+		const name = e.target.name
+		const value = e.target.value
+		const data = {
+			key,
+			page,
+			name,
+			value
+		}
 
-	updateSession(data)
-}))
+		updateSession(data)
+	}, 250)
+
+	allInputs.forEach(input => input.addEventListener('input', onInput))
+}())
+
+
+
+
+
+
+
 
 function getStoredData(key) {
 	return JSON.parse(localStorage.getItem(key))
@@ -29,3 +40,21 @@ function updateSession(data) {
 	session[data.page][data.name] = data.value
 	storeData(data.key, session)
 }
+
+
+//Debounce function taken from: https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+	var timeout
+	return function () {
+		var context = this,
+			args = arguments
+		var later = function () {
+			timeout = null
+			if (!immediate) func.apply(context, args)
+		};
+		var callNow = immediate && !timeout
+		clearTimeout(timeout)
+		timeout = setTimeout(later, wait)
+		if (callNow) func.apply(context, args)
+	};
+};
