a85f2a69d2719d7b28b8a2ed12e9d9b4a89e05f6
Wessel Smit <wrsmit00@gmail.com>
2020-03-25 13:13:32 +0100

    session key now automatically gets selected + key is stored in localStorage

---
+++ static/scripts/newSession.js
@@ -0,0 +1,43 @@
+//Auto select the key-text to make it easier to copy for the user
+const keyCodeElement = document.getElementById('keyCode')
+keyCodeElement.select()
+
+
+
+//Check if localStorage is both supported & available (enabled)
+if (localStorageAvailable()) {
+	console.log('localStorage is supported and available')
+	const keyCode = keyCodeElement.value
+	localStorage.setItem('key', keyCode)
+} else {
+	console.log('localStorage is not supported or not available')
+}
+
+
+
+
+/* Best way to detect if localStorage is supported & available (taken from MDN: 
+	https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Testing_for_availability) */
+function localStorageAvailable() {
+	let storage
+	try {
+		storage = window['localStorage']
+		const x = '__storage_test__'
+		storage.setItem(x, x)
+		storage.removeItem(x)
+		return true;
+	} catch (err) {
+		return err instanceof DOMException && (
+				// everything except Firefox
+				err.code === 22 ||
+				// Firefox
+				err.code === 1014 ||
+				// test name field too, because code might not be present
+				// everything except Firefox
+				err.name === 'QuotaExceededError' ||
+				// Firefox
+				err.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+			// acknowledge QuotaExceededError only if there's something already stored
+			(storage && storage.length !== 0)
+	}
+}
