ee1e5c5d505d85a70d9a5681ec68a43849083068
Wessel Smit <wrsmit00@gmail.com>
2020-03-26 10:40:21 +0100

    fixed storage saving function by moving it one page forwards

--- routes/new.js
+++ routes/new.js
@@ -1,7 +1,35 @@
+const storage = require('#modules/storage.js')
 const uid = require('uid')
 
 module.exports = (req, res) => {
 	const key = uid(7)
+
+	const obj = {
+		"key": key,
+		"person": {
+			"first_name": "",
+			"surname": "",
+			"gender": "",
+			"age": "",
+			"github": ""
+		},
+		"favorites": {
+			"browser": "",
+			"code_editor": "",
+			"development": "",
+			"indentation": "",
+			"language": ""
+		},
+		"openQuestions": {
+			"tip": "",
+			"change": "",
+			"upcoming": ""
+		}
+	}
+
+	//todo: dit soort paths gaan kapot op heroku
+	// storage.saveJSON(obj, `./storage/${key}.json`)
+	storage.saveJSON(obj, `storage/${key}.json`)
 
 	console.log('starting new session, key is: ', key)
 	res.render('new', {
