31d53f9884ae8abddc3b3bd43b82cd35ea39db55
Wessel Smit <wrsmit00@gmail.com>
2020-03-28 16:35:12 +0100

    Update enhanceAnim.js

--- static/scripts/enhanceAnim.js
+++ static/scripts/enhanceAnim.js
@@ -2,26 +2,5 @@
 const animatedEl = document.querySelector('.fadeAnim')
 animatedEl.classList.remove('fadeAnim')
 
-//Add enhanced animation intro
-animatedEl.classList.add('enhancedAnimIn')
 
-//Add enhanced animation outro
-const navButtons = document.querySelectorAll('a, input[type=submit]')
-for (const button of navButtons) {
-	button.addEventListener('click', function (e) {
-		var form = document.querySelector('form')
-		e.preventDefault()
-
-		form.classList.add('enhancedAnimOut')
-		if (e.target.tagName === 'A') {
-			var link = e.target.href
-			setTimeout(function () {
-				window.location.href = link
-			}, 500)
-		} else {
-			setTimeout(function () {
-				form.submit()
-			}, 500)
-		}
-	})
-}
+//todo: hier moet de enhanced animation komen
