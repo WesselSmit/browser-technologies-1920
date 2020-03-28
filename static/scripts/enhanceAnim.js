3a568cc9763496a62af78811530902c2dce88d0a
Wessel Smit <wrsmit00@gmail.com>
2020-03-28 21:01:32 +0100

    better required design  + removed some old css + js

--- static/scripts/enhanceAnim.js
+++ static/scripts/enhanceAnim.js
@@ -1,6 +1,6 @@
 //Remove ordinary fade-in animation
-const animatedEl = document.querySelector('.fadeAnim')
-animatedEl.classList.remove('fadeAnim')
+// const animatedEl = document.querySelector('.fadeAnim')
+// animatedEl.classList.remove('fadeAnim')
 
 
 //todo: hier moet de enhanced animation komen
--- static/scripts/progress.js
+++ static/scripts/progress.js
@@ -94,31 +94,25 @@ function validation() {
 	const allInputs = form.querySelectorAll('input:not([type=hidden]):not([type=submit]), textarea, select')
 	for (const input of allInputs) {
 		input.classList.remove('required')
-		document.querySelector(`[for=${input.id}]`).classList.remove('requiredLabel')
 		if (input.type === 'text') {
 			if (input.value === "") {
 				input.classList.add('required')
-				document.querySelector(`[for=${input.id}]`).classList.add('requiredLabel')
 			}
 		} else if (input.tagName === 'SELECT') {
 			const selectEl = document.querySelector('select')
 			if (selectEl.value === '') {
 				selectEl.classList.add('required')
-				document.querySelector(`[for=${input.id}]`).classList.add('requiredLabel')
 			}
 		} else if (input.tagName === 'TEXTAREA') {
 			if (input.value === '') {
 				input.classList.add('required')
-				document.querySelector(`[for=${input.id}]`).classList.add('requiredLabel')
 			}
 		} else if (input.type === 'radio') {
 			const radioContainer = input.parentElement
 			radioContainer.classList.add('requiredField')
-			radioContainer.querySelector('legend').classList.add('requiredLabel')
 			for (const radio of radioContainer.querySelectorAll('input[type=radio')) {
 				if (radio.checked) {
 					radioContainer.classList.remove('requiredField')
-					radioContainer.querySelector('legend').classList.remove('requiredLabel')
 				}
 			}
 		}
